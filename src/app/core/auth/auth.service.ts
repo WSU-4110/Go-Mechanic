e4073167a839f733cdb8815b4346cc2d1831b4b8
JimDUc <gz8751@wayne.edu>
2022-10-12 16:51:34 -0400

    Implemented login authentication, once logged in sign up button will be removed login button will be removed logout button will be shown

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import {
+  Auth, signInWithEmailAndPassword, authState} from '@angular/fire/auth';
+import { from, Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  currentUser$ = authState(this.auth);
 
-  constructor() { }
+  constructor(private auth: Auth) {}
+
+
+  login(email: string, password: string): Observable<any> {
+    return from(signInWithEmailAndPassword(this.auth, email, password));
+  }
+
+  logout(){
+    return from(this.auth.signOut());
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-if (environment.firebase.production) {
+if (environment.production) {
   enableProdMode();
 }
 
