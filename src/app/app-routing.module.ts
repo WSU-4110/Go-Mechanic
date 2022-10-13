3ea33967558af71a4a6c844eb765f984a8c1c4af
Joseph Brooks <gx0343@wanye.edu>
2022-10-12 22:41:39 -0400

    Added navbar Logo, updated navbar components,created maintenance page outline.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutpageComponent } from './public/components/aboutpage/aboutpage.component';
+import { BrakejobpageComponent } from './public/components/brakejobpage/brakejobpage.component';
 import { ContactpageComponent } from './public/components/contactpage/contactpage.component';
+import { EnginelightpageComponent } from './public/components/enginelightpage/enginelightpage.component';
 import { HomepageComponent } from './public/components/homepage/homepage.component';
 import { LoginpageComponent } from './public/components/loginpage/loginpage.component';
+import { MaintenancepageComponent } from './public/components/maintenancepage/maintenancepage.component';
 import { ReportissuepageComponent } from './public/components/report-issue-page/reportissuepage.component';
+import { SearchrepairspageComponent } from './public/components/searchrepairspage/searchrepairspage.component';
 import { SignupComponent } from './public/components/signup/signup.component';
 
 /** Use whatever routing is here in the public-routing module if we end up not use this base level one. */
@@ -36,8 +40,27 @@ const routes: Routes = [
   {
     path: 'signup',
     component: SignupComponent
-  }
+  },
 
+  {
+    path: 'enginelightpage',
+    component: EnginelightpageComponent
+  },
+
+  {
+    path: 'brakejobpage',
+    component: BrakejobpageComponent
+  },
+
+  {
+    path: 'searchrepairspage',
+    component: SearchrepairspageComponent
+  },
+
+  {
+    path: 'maintenancepage',
+    component: MaintenancepageComponent
+  }
 ];
 
 @NgModule({
