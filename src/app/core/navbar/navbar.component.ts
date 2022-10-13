25e833dcac19f404ae7b7b36df7844966ff94de9
Joseph Brooks <gx0343@wanye.edu>
2022-10-13 02:10:58 -0400

    Implemented account registration page, account management in firebase

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { AuthService } from 'src/app/core/auth/auth.service';
+import { AuthenticationService } from 'src/app/core/auth/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +10,7 @@ import { AuthService } from 'src/app/core/auth/auth.service';
 export class NavbarComponent implements OnInit {
 
   constructor(
-    public authService: AuthService,
+    public authService: AuthenticationService,
     private router: Router
     ) { }
 
