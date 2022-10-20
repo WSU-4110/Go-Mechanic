3b00cce7b26682324a3033e54f2ea04f8705969b
Joseph Brooks <gx0343@wanye.edu>
2022-10-19 20:02:37 -0400

    pipe error

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@angular/core';
-import {Auth, signInWithEmailAndPassword, authState} from '@angular/fire/auth';
-import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { Injectable, NgZone } from '@angular/core';
+import {Auth, signInWithEmailAndPassword, authState, updateProfile, createUserWithEmailAndPassword} from '@angular/fire/auth';
+import { AngularFireAuth } from '@angular/fire/compat/auth'; //Implementing verification email service
+import { Router } from '@angular/router';
 import { switchMap,from } from 'rxjs';
 
 @Injectable({
@@ -9,11 +10,41 @@ import { switchMap,from } from 'rxjs';
 export class AuthenticationService {
   currentUser$ = authState(this.auth);
 
-  constructor(private auth: Auth) {}
+  constructor(
+    private auth: Auth,
+    public afAuth: AngularFireAuth, //Inject Firebase auth service
+    public router : Router, //inject route service
+    public ngZone : NgZone, //NgZone service removes outsid scope warning
+    ) {}
+
+//Email Verification Method
+SendVerificationMail(){
+  return this.afAuth.currentUser
+  .then((user) => {
+    return user?.sendEmailVerification();
+  })
+  .then(() => {
+    this.router.navigate(['verify-email-address']);
+  });
+}
 
 
   login(email: string, password: string){
-    return from(signInWithEmailAndPassword(this.auth, email, password));
+    return this.afAuth
+    .signInWithEmailAndPassword(email, password)
+    .then((result) => {
+      if (result.user?.emailVerified !== true) {
+        this.SendVerificationMail();
+        window.alert(
+          'Please validate your email address, check your inbox!'
+        );
+      } else {
+        this.ngZone.run(() => {
+          this.router.navigate(['<!-- enter your route name here -->']);
+        });
+      }
+    })
+    .catch(err => console.log(err));
   }
 
   logout(){
@@ -21,6 +52,11 @@ export class AuthenticationService {
   }
 
   signUp(name: string, email: string, password: string){
-    return from(createUserWithEmailAndPassword(this.auth, email, password))
+    return this.afAuth.createUserWithEmailAndPassword(email, password)
+
+    .then((result)=>{
+    this.SendVerificationMail()
+    })
+    .catch(err => console.log(err));
   }
 }
