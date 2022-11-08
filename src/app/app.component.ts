132e5f80cbf63bb17fcac1ad1771ecb9e5080183
Anthony Quinteros <gx1350@wayne.edu>
2022-11-07 19:43:35 -0500

    Fixing bugged code
    Fixing bugged code.

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+
+/** templateUrl: './app.component.html', */
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'GoMechanicDev';
+}
